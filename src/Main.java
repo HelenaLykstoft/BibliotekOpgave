b0cb67183d8ba1630df53b57b6881f697f34bb8f
Felicia Favrholdt <feliciafavrholdt93@gmail.com>
2022-09-19 16:43:02 +0200

    adds switch case to main and updates Facade file

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
+import Niveau2_logik.TerminalInput;
 import Niveau3_persistens.DatabaseConnection;
+import Niveau3_persistens.Facade;
 
 public class Main {
     public static void main(String[] args) {
-        DatabaseConnection connection = new DatabaseConnection();
+ /*       DatabaseConnection databaseConnection = new DatabaseConnection();
+        databaseConnection.createConnection();*/
+
+        Facade facade = new Facade();
+
+        int input;
+        String s = "What do you wish to do?";
+
+        while (true) {
+            input = TerminalInput.getInt(s);
+            switch (input) {
+                case 1:
+                    facade.createCustomer();
+                    break;
+                case 2:
+                    facade.updateCustomer();
+                    break;
+                case 3:
+                    facade.showCustomers();
+                    break;
+                default:
+                    System.out.println("Something went wrong.. Try again");
+            }
+        }
+    }
+
+
     }
 }
